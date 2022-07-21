37eb2417b3cafbd64ce1d5faa04a1276fa23145d
Hyo <dooboolab@gmail.com>
2022-07-21 21:21:52 +0900

    Update packages and migrate to Prisma 4.1.0 (#3)
    * Update package.json
    
    * Chnage pkg manager to yarn
    
    * Upgrade yarn to yarn berry
    
    * Resolve point-of-view types
    
    * Remove pnpm from CI and replace to yarn
    
    * Use ts-ignore for `reply.view`

--- src/apis/third-party.ts
+++ src/apis/third-party.ts
@@ -2,6 +2,7 @@ import {FastifyPluginAsync} from 'fastify';
 
 const thirdPartyRoute: FastifyPluginAsync = async (server) => {
   server.get('/daum_address', async (req, reply) => {
+    // @ts-ignore
     reply.view('daum_address');
   });
 };
