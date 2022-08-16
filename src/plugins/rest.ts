791dc1e8b112c3a63129b9662a715b4d2b949c54
hyochan <dooboolab@gmail.com>
2022-08-16 22:51:06 +0900

    feat(migration): replace deprecated `point-of-view` with `view`

--- src/plugins/rest.ts
+++ src/plugins/rest.ts
@@ -1,8 +1,8 @@
 import {FastifyPluginAsync} from 'fastify';
 import apis from '../apis';
 import ejs from 'ejs';
+import fastifyView from '@fastify/view';
 import path from 'path';
-import pointOfView from 'point-of-view';
 
 const restPlugin: FastifyPluginAsync = async (server) => {
   server.register(pointOfView, {
